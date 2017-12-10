15b9b21730af3ae5b8de4e4cb021883e605907ce
hideki <andre.hkiw@gmail.com>
2017-12-10 10:17:40 -0200

    Implementação do bottom nav view

--- app/src/main/java/com/adoptcat/adoptcat/fragments/AccountFragment.java
+++ app/src/main/java/com/adoptcat/adoptcat/fragments/AccountFragment.java
@@ -1,10 +1,27 @@
 package com.adoptcat.adoptcat.fragments;
 
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.adoptcat.adoptcat.R;
 
 public class AccountFragment extends Fragment {
 
 
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.activity_maps_fragment, null);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
 }
